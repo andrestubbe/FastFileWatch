439096e77f398af289ac7e264b5f5e5444b1d9b8
Andre Stubbe <andrestubbe@proton.me>
2026-06-07 13:13:38 +0200

    Auto Update

--- run-demo.bat
+++ run-demo.bat
@@ -3,6 +3,6 @@ chcp 65001 >nul
 cd /d "%~dp0"
 echo [FastFileWatch] Running Demo (via JitPack)...
 cd examples\Demo
-call mvn compile exec:java -Dexec.mainClass=fastfilewatch.Demo
+call mvn -q compile exec:java -Dexec.mainClass=fastfilewatch.Demo
 cd ..\..
 pause
