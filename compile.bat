9430e2ceeb8382938ebf4e0b1162c5b05d09dc5c
Andre Stubbe <andrestubbe@proton.me>
2026-08-29 11:24:25 +0200

    Auto Update

--- compile.bat
+++ compile.bat
@@ -1,4 +1,5 @@
 @echo off
-set JAVA_HOME=C:\Program Files\Java\jdk-25
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-cl /LD /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /Fe:build\fastfilewatch.dll native\FastFileWatch.cpp /link advapi32.lib
+set JAVA_HOME=C:\Program Files\Java\jdk-21.0.12.1
+call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
+if not exist build mkdir build
+cl /LD /EHsc /O2 /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /Fe:build\fastfilewatch.dll native\FastFileWatch.cpp /link advapi32.lib
